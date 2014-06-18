74b5a02f2526595c20cdde53cb892170f8b5760d
Christian Hoppe <Christian.Hoppe@Hotmail.de>
2014-06-18 11:50:15 +0200

    added FilterPlugin for FreeBase

---
+++ FilterPluginFreeBase/CFilterPluginFreeBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AASDSearch.Filter
+{
+    public class CFilterPluginFreeBase : AASDSearch.Common.IFilterPlugin
+    {
+        public string Name
+        {
+            get { return "Filter using FreeBase"; }
+        }
+        public string Desc
+        {
+            get { return "This Filter Pluigin uses FreeBase to filter the search results."; }
+        }
+        public Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+        public void process(AASDSearch.Common.CSearchRequest pSearchrequest)
+        {
+            //  Filter functionallity does nothing
+
+        }
+    }
+}
+
---
+++ FilterPluginFreeBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("FilterPluginFreeBase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FilterPluginFreeBase")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("37fd9d16-d856-4396-8cc5-a4e1d90e083c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
